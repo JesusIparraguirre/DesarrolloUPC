45259e6e59abd454b45339c38c5e3e67909607e5
Jesus <jesusiparraguirre1998@gmail.com>
2020-04-25 17:26:40 -0500

    error

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   get 'login/index', as: 'login'
   get 'productos/index'
   get 'productos/show'
-  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.
   
   root 'productos#index'
